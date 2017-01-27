5833cc98582574e99851f5faaba2bb5c58dca968
Arty <Arty>
2017-01-28 00:49:06 +0200

    Added networking

---
+++ MultiplayerV2/Assets/PlayerSetup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        //Check if local player
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                //Disable all of components to dissable 
+                componentsToDisable[i].enabled = false;
+            }
+        }else //Dissable main camera from the game view 
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+        
+    }
+
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+
+
+
+
+}
